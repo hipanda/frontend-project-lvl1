b7ffb63e4eaf3374cab366449ac34720edac29f3
Vasiliy Pugovkin <hipanda@gmail.com>
2020-01-04 18:58:36 +0300

    Refactored even-game loop to iterative function

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -8,14 +8,19 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
 const name = askName();
 
 const NEEDED_ANSWERS = 3;
-let correctAnswers = 0;
 
-while (correctAnswers < NEEDED_ANSWERS) {
+const iterQuestion = (correctAnswers) => {
+  if (correctAnswers === NEEDED_ANSWERS) {
+    return console.log(`Congratulations, ${name}!\n`);
+  }
+
   const isCorrect = askEvenNumber();
   if (isCorrect) {
-    correctAnswers += 1;
-  } else {
-    console.log(`Let's try again, ${name}!\n`);
+    return iterQuestion(correctAnswers + 1);
   }
-}
-console.log(`Congratulations, ${name}!\n`);
+
+  console.log(`Let's try again, ${name}!\n`);
+  return iterQuestion(correctAnswers);
+};
+
+iterQuestion(0);

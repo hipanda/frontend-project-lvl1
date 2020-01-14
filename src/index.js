39ed00f255d8564ac45e72a976228b5899f81aed
Vasiliy Pugovkin <hipanda@gmail.com>
2020-01-15 01:49:37 +0300

    Removed quizPlayer

--- src/index.js
+++ src/index.js
@@ -5,29 +5,8 @@ import { getQuest, getKey } from './utils/task';
 // number of correct answers to win. common for all games
 const answersToWin = 3;
 
-// common question iterator
-const quizPlayer = (playerName, taskFunc, correctAnswersNum = 0) => {
-  if (correctAnswersNum === answersToWin) {
-    return console.log(`Congratulations, ${playerName}!\n`);
-  }
-
-  const task = taskFunc();
-  const correctAnswer = getKey(task);
-  const playerAnswer = readlineSync.question(`Question: ${getQuest(task)}\nYour answer: `);
-
-  const isCorrect = correctAnswer === playerAnswer;
-  if (isCorrect) {
-    console.log('Correct!\n');
-    return quizPlayer(playerName, taskFunc, correctAnswersNum + 1);
-  }
-
-  console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-  console.log(`Let's try again, ${playerName}!\n`);
-  return false;
-};
-
 // playGame
-export default (game, rules) => {
+export default (gameTaskFunc, rules) => {
   // greetigns
   console.log('\nWelcome to the Brain Games!');
   // ask name
@@ -35,5 +14,24 @@ export default (game, rules) => {
   console.log(`Hello, ${name}!\n`);
 
   console.log(`${rules}\n`);
-  quizPlayer(name, game);
+
+  let correctAnswersNum = 0;
+  while (correctAnswersNum < answersToWin) {
+    const task = gameTaskFunc();
+    const correctAnswer = getKey(task);
+    const playerAnswer = readlineSync.question(`Question: ${getQuest(task)}\nYour answer: `);
+
+    const isCorrect = correctAnswer === playerAnswer;
+
+    if (isCorrect) {
+      correctAnswersNum += 1;
+      console.log('Correct!\n');
+    } else {
+      console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${name}!\n`);
+      return false;
+    }
+  }
+
+  return console.log(`Congratulations, ${name}!\n`);
 };

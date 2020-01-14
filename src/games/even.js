ebf3d4a5254afbd13d71e7c0ce0fb4e9d89a3b40
Vasiliy Pugovkin <hipanda@gmail.com>
2020-01-15 01:21:16 +0300

    isEven is now a predicate function

--- src/games/even.js
+++ src/games/even.js
@@ -5,11 +5,12 @@ import random from '../utils/random';
 
 export const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => num % 2 === 0;
+
 const even = () => {
   const number = random(1, 100);
-  const isEven = number % 2 === 0;
 
-  const correctAnswer = isEven ? 'yes' : 'no';
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
   return task(number, correctAnswer);
 };
 
